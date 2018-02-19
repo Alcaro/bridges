91c2567c13d4cbfedf1840f57212e5e7b7623690
Alcaro <floating@muncher.se>
2018-02-19 14:24:55 +0100

    must be more faster

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
+#define MARCH2018
 #include "arlib.h"
 
-//int i=0;
 class solver {
 	int width;
 	int height;
 	int size;
 	
 	int not_ocean; // index to an arbitrary island
+	int n_islands;
 	
 	array<int8_t> map; // number of bridges, 1-8, or -1 for ocean
 	                   // (0 is legal too, but unless that's the only non-ocean, that map is impossible.)
@@ -14,15 +15,18 @@ class solver {
 	                       // 1<<(1,2,3) means up, left and down, respectively
 	                       // the 1<<(4,5,6,7) bits mean it's possible to have 1 bridge in that direction
 	                       // 1<<(8..11) means 2 bridges
-	                       // 1<<(12..15) are unused, always 0
-	                       // (TODO: may be usable for temp stuff, or a 'is ocean' flag)
+	                       // 1<<12 is used to check whether all islands are connected; outside that function, always false
+	                       // 1<<(13..15) are completely unused
 	                       // ocean tiles have this set too; for them, left and right are always equal, as are up/down
+	
+	array<int> towalk;
+	int n_towalk;
 	
 	//Sets state[ix] to newbits, and updates the corresponding bits for the connecting islands.
 	//Can only clear bits, can't set.
-	//If call_fill_known is set, calls fill_known for all changed tiles.
-	//Returns false if any call_fill_known does.
-	bool set_state(int ix, uint16_t newstate, bool call_fill_known_at = false)
+	//If call_fill_known is set, calls fill_known_at for all changed tiles.
+	//Returns false if any fill_known_at does.
+	bool set_state(int ix, uint16_t newstate)
 	{
 		uint16_t oldstate = state[ix];
 		
@@ -45,32 +49,29 @@ class solver {
 			}
 		}
 		
-		if (call_fill_known_at)
+		int bank = ix / size;
+		ix %= size;
+		
+		for (int dir=0;dir<4;dir++)
 		{
-			int bank = ix / size;
-			ix %= size;
-			
-			for (int dir=0;dir<4;dir++)
+			int offset = offsets[dir];
+			if ((oldstate^newstate) & (0x0111<<dir))
 			{
-				int offset = offsets[dir];
-				if ((oldstate^newstate) & (0x0111<<dir))
+				//don't merge those calls, it's a tiny slowdown for whatever reason
+				int ix2 = ix + offset;
+				while (map[ix2] == -1)
 				{
-					//don't merge those calls, it's a tiny slowdown for whatever reason
-					int ix2 = ix + offset;
-					while (map[ix2] == -1)
-					{
-						if (!fill_known_at(bank, ix2)) return false;
-						ix2 += offset;
-					}
 					if (!fill_known_at(bank, ix2)) return false;
+					ix2 += offset;
 				}
+				if (!fill_known_at(bank, ix2)) return false;
 			}
 		}
 		
 		return true;
 	}
 	
-	//Returns only popcount of the middle nybble.
+	//Returns popcount of the middle nybble.
 	__attribute__((always_inline)) int popcount_mid(uint16_t in)
 	{
 		in -= ((in>>1) & 0x0550);
@@ -78,7 +79,8 @@ class solver {
 		return ((in + (in>>4)) & 0x0F0F0F0F) * 0x01010101 >> 24;
 	}
 	
-	//Returns false if map is now known impossible.
+	//Removes any possibilities from the given index that would create crossing bridges, or require more or less bridges than that tile allows.
+	//Returns false if that map is now known unsolvable. If so, the state is now undefined; if not, the state is valid.
 	bool fill_known_at(int bank, int ix)
 	{
 		int required = map[ix];
@@ -99,102 +101,122 @@ class solver {
 			if (min > required || max < required) return false;
 			if (min == max) return true; // no options left
 			
-			//if no further bridges can be built, do that
-			if (min == required) return set_state(ix, flags_min, true);
+			//if no further bridges can be built, ban the options
+			if (min == required) return set_state(ix, flags_min);
 			//if all possible bridges must be built, do that
-			if (max == required) return set_state(ix, (flags & ~(flags>>4 | flags>>8)), true);
+			if (max == required) return set_state(ix, (flags & ~(flags>>4 | flags>>8)));
 			
 			//if only one more bridge is free, lock all two-bridge possibilities
-			//(with a continue above, to avoid putting some bridges back)
 			if (min == max-1) return true; // no two-bridge possibilities open
 			
 			uint16_t flags_min_minus1 = (flags & ~(flags<<8));
 			uint16_t flags_max_minus1 = (flags & ~(flags>>8));
-			if (min == required-1 && flags_min_minus1 != flags) return set_state(ix, flags_min_minus1, true);
-			if (max == required+1 && flags_max_minus1 != flags) return set_state(ix, flags_max_minus1, true);
+			if (min == required-1 && flags_min_minus1 != flags) return set_state(ix, flags_min_minus1);
+			if (max == required+1 && flags_max_minus1 != flags) return set_state(ix, flags_max_minus1);
 		}
 		else
 		{
 			//ocean tile with mandatory vertical bridge and allowed horizontal -> require zero horizontal
 			if ((flags & 0x0550) != 0 && (flags & 0x000A) == 0)
 			{
-				return set_state(ix, flags & 0x0AAF, true);
+				return set_state(ix, flags & 0x0AAF);
 			}
 			//mandatory horizontal -> ban vertical
 			if ((flags & 0x0AA0) != 0 && (flags & 0x0005) == 0)
 			{
-				return set_state(ix, flags & 0x055F, true);
+				return set_state(ix, flags & 0x055F);
 			}
 		}
 		return true;
 	}
 	
-	//Removes any possibilities that would create crossing bridges, or require more or less bridges than that tile allows.
-	//Returns false if that map is now known unsolvable. If so, the state is now undefined; if not, the state is valid.
-	bool fill_known(int bank)
+	//Returns whether the map is disjoint, i.e. some islands aren't connected to each other.
+	bool is_disjoint(int bank)
 	{
+		int banksize = bank*size;
+		
+		n_towalk = 1;
+		state[banksize + not_ocean] |= 0x1000;
+		towalk[0] = not_ocean;
+		
+		int visited = 0;
+		while (n_towalk)
+		{
+			int ix = towalk[--n_towalk];
+			visited++;
+			
+			int flags = state[banksize + ix];
+			
+			//unrolled for performance
+			
+			//right
+			if (flags & 0x0110)
+			{
+				int ix2 = ix;
+				do {
+					ix2 += 1;
+				} while (map[ix2] == -1);
+				
+				if ((state[banksize + ix2] & 0x1000) == 0)
+				{
+					//eagerly mark visited, so the ocean paths aren't traversed twice
+					state[banksize + ix2] |= 0x1000;
+					towalk[n_towalk++] = ix2;
+				}
+			}
+			
+			//up
+			if (flags & 0x0220)
+			{
+				int ix2 = ix;
+				do {
+					ix2 += -width;
+				} while (map[ix2] == -1);
+				
+				if ((state[banksize + ix2] & 0x1000) == 0)
+				{
+					state[banksize + ix2] |= 0x1000;
+					towalk[n_towalk++] = ix2;
+				}
+			}
+			
+			//left
+			if (flags & 0x0440)
+			{
+				int ix2 = ix;
+				do {
+					ix2 += -1;
+				} while (map[ix2] == -1);
+				
+				if ((state[banksize + ix2] & 0x1000) == 0)
+				{
+					state[banksize + ix2] |= 0x1000;
+					towalk[n_towalk++] = ix2;
+				}
+			}
+			
+			//down
+			if (flags & 0x0880)
+			{
+				int ix2 = ix;
+				do {
+					ix2 += width;
+				} while (map[ix2] == -1);
+				
+				if ((state[banksize + ix2] & 0x1000) == 0)
+				{
+					state[banksize + ix2] |= 0x1000;
+					towalk[n_towalk++] = ix2;
+				}
+			}
+		}
+		if (visited != n_islands) return true;
+		
 		int ix=size;
 		do
 		{
 			ix--;
-			if (!fill_known_at(bank, ix)) return false;
-		} while (ix);
-		
-		if (is_disjoint(bank)) return false;
-		return true;
-	}
-	
-	//Returns whether the map is disjoint, i.e. some bridges aren't connected to each other.
-	bool is_disjoint(int bank)
-	{
-		int ix = bank*size + not_ocean;
-		
-		//I have four bits of unused state per tile, one per direction. All start out clear.
-		//I will start at an arbitrary tile, facing an arbitrary direction.
-		//If that path is blocked (no bridges can be built there), I'll turn right until it's not blocked.
-		//I will then mark the path I'm facing as blocked, then follow it.
-		//Once I arrive at the target, I will check if I've been here before (if any path is marked blocked).
-		//If yes, I'll return to where I came from. If no, I'll turn left.
-		//In both cases, I'll then repeat from the 'if that path is blocked' clause.
-		//Once all four paths are taken, I will stop moving, and instead check all tiles on the map.
-		//If any tile has an existing but untaken path, I will report the map unsolvable. Otherwise, I will clear all such flags.
-		
-		int dir = 0;
-		int offsets[4] = { 1, -width, -1, width };
-		//while there is an untaken but possible path from here
-		while (true)
-		{
-			int targets = (~state[ix])&0xF000 & (state[ix]<<4 | state[ix]<<8);
-			if (!targets) break;
-			//rotate until path is walkable
-			while (!(targets & (0x1000<<dir))) dir = (dir+1) & 3;
-			
-			//mark blocked
-			state[ix] |= 0x1000<<dir;
-			
-			ix -= bank*size;
-			//follow
-			do {
-				ix += offsets[dir];
-			} while (map[ix] == -1);
-			ix += bank*size;
-			
-			//if I've been here already, and it's possible to turn around, do that
-			if ((state[ix]&0xF000) && !(state[ix]&(0x1000<<(dir^2)))) dir = dir^2;
-			//otherwise, turn left
-			else dir = (dir+3) & 3;
-		}
-		
-		ix=size;
-		do
-		{
-			ix--;
-			int ix2 = bank*size + ix;
-			//untaken but possible path = not connected = unsolvable
-			if (map[ix]!=-1 && ((~state[ix2])&0xF000 & (state[ix2]<<4 | state[ix2]<<8)))
-			{
-				return true;
-			}
+			int ix2 = banksize + ix;
 			state[ix2] &= 0x0FFF;
 		} while (ix);
 		
@@ -224,12 +246,12 @@ class solver {
 	
 	int solve_rec(int depthat, int maxdepth)
 	{
-	fill_again:
-		if (!fill_known(depthat)) return 0;
+		if (is_disjoint(depthat)) return 0;
 		if (finished(depthat)) return 1;
 		
 		if (depthat == maxdepth) return -1;
 		
+	fill_again:
 		bool didsomething = false;
 		
 		for (int y=0;y<height;y++)
@@ -242,7 +264,7 @@ class solver {
 			uint16_t flags = state[ix];
 			
 			//right and up (ignore left/down, they'd only find duplicates)
-			for (int dir=0;dir<2;dir++)
+			for (int dir=0;dir<=1;dir++)
 			{
 				//if multiple bridge numbers are allowed in this direction
 				if (flags & (flags>>4 | flags>>8) & (0x0111<<dir))
@@ -255,15 +277,16 @@ class solver {
 						if (flags & dirbit)
 						{
 							copy_bank(depthat+1, depthat);
-							set_state(ix_next, flags & (set_mask | dirbit));
-							int ret = solve_rec(depthat+1, maxdepth);
+							int ret;
+							if (!set_state(ix_next, flags & (set_mask | dirbit))) ret = 0;
+							else ret = solve_rec(depthat+1, maxdepth);
 							if (ret == -1) n_solvable = -99;
 							if (ret == 0)
 							{
-								set_state(ix, state[ix] & ~dirbit);
+								if (!set_state(ix, state[ix] & ~dirbit)) return 0;
 								didsomething = true;
 								
-								if (!fill_known(depthat)) return 0;
+								if (is_disjoint(depthat)) return 0;
 								if (finished(depthat)) return 1;
 							}
 							if (ret == 1) n_solvable++;
@@ -272,7 +295,7 @@ class solver {
 					}
 					if (n_solvable > 1) return 2;
 					
-					if (!(state[ix] & (0x0111<<dir))) return 0; // all options are impossible
+					if (!(state[ix] & (0x0111<<dir))) return 0; // if all options are impossible, drop it
 				}
 			}
 		}
@@ -294,8 +317,11 @@ public:
 		while (inmap[(width+1) * height] != '\0') height++;
 		size = height * width;
 		
+		n_islands = 0;
+		
 		map.resize(size);
 		state.resize(size);
+		towalk.resize(size);
 		
 		for (int y=0;y<height;y++)
 		for (int x=0;x<width;x++)
@@ -309,6 +335,8 @@ public:
 			state[y*width + x] = 0x000F; // all tiles can have 0 bridges towards anywhere, including oceans and things pointing outside the map
 			if (neighbors != -1)
 			{
+				n_islands++;
+				
 				not_ocean = y*width + x;
 				for (int x2=x-1;x2>=0;x2--)
 				{
@@ -356,6 +384,13 @@ public:
 	int solve(int maxdepth = 3)
 	{
 		state.reserve(size*(maxdepth+1));
+		
+		for (int ix=0;ix<size;ix++)
+		{
+			if (!fill_known_at(0, ix)) return 0;
+		}
+		if (is_disjoint(0)) return 0;
+		
 		for (int depth = 0; depth <= maxdepth; depth++)
 		{
 			int ret = solve_rec(0, depth);
@@ -462,6 +497,7 @@ static void test_multistep(cstring test)
 
 test("solver", "", "solver")
 {
+#if 1
 	testcall(test_one( // just something simple
 		" 2 \n" /* */ " 0 \n"
 		"271\n" /* */ "210\n"
@@ -486,14 +522,21 @@ test("solver", "", "solver")
 		"22\n" /* */ "10\n"
 		"22\n" /* */ "10\n"
 	));
-	testcall(test_one( // these cycles may confuse the island tracker
+#endif
+	testcall(test_one( // simplified version of the below
+		"2332\n" /* */ "1110\n"
+		"1 1 \n" /* */ "0 0 \n"
+		" 1 1\n" /* */ " 0 0\n"
+	));
+#if 1
+	testcall(test_one( // these branches may confuse the island tracker
 		"1 2  \n" /* */ "1 0  \n"
 		" 1 1 \n" /* */ " 0 0 \n"
 		"14441\n" /* */ "11110\n"
 		" 1 1 \n" /* */ " 0 0 \n"
 		"  2 1\n" /* */ "  1 0\n"
 	));
-	testcall(test_one( // a few cycles, to confuse the island tracker
+	testcall(test_one( // a few cycles too, must confuse the island tracker
 		"4    4 \n" /* */ "2    0 \n"
 		"  4 4  \n" /* */ "  2 0  \n"
 		"    682\n" /* */ "    220\n"
@@ -562,6 +605,7 @@ test("solver", "", "solver")
 		"32 3\n" /* */ "11 0\n"
 		"2  1\n" /* */ "1  0\n"
 	));
+#endif
 }
 
 
@@ -597,33 +641,64 @@ class generator {
 		}
 	}
 	
-	static void add_island(string& ret, int width, int y, int x)
+	static bool add_island(string& ret, int width, int y, int x)
 	{
-		int nbridge = 1 + rand()%2;
-		switch (ret[(width+1)*y + x])
-		{
-			case '>':
-				fill_until(ret, width, y, x, 0, '#', nbridge);
-				break;
-			case '^':
-				fill_until(ret, width, y, x, 1, '#', nbridge);
-				break;
-			case '<':
-				fill_until(ret, width, y, x, 2, '#', nbridge);
-				break;
-			case 'v':
-				fill_until(ret, width, y, x, 3, '#', nbridge);
-				break;
-			case ' ':
-				nbridge = 0;
-				break;
-		}
-		ret[(width+1)*y + x] = '0'+nbridge;
+		int ix = (width+1)*y + x;
+		bool first = (ret[ix] == ' ');
 		
-		fill_until(ret, width, y, x, 0, '<');
-		fill_until(ret, width, y, x, 1, 'v');
-		fill_until(ret, width, y, x, 2, '>');
-		fill_until(ret, width, y, x, 3, '^');
+		int offsets[4] = { 1, -(width+1), -1, width+1 };
+		
+		int allowed = 0;
+		
+		for (int dir=0;dir<4;dir++)
+		{
+			int ix2 = ix;
+			int offset = offsets[dir];
+			while (true)
+			{
+				ix2 += offset;
+				if (ix2 < 0 || (unsigned)ix2 >= ret.length() || ret[ix2] == '\n' || ret[ix2] == '#')
+				{
+					break;
+				}
+				if (isdigit(ret[ix2]))
+				{
+					allowed |= 1<<dir;
+					break;
+				}
+			}
+		}
+		
+		char newtile = '0';
+		if (allowed == 0 && !first)
+		{
+			ret[ix] = ' ';
+			return false;
+		}
+		if (allowed != 0)
+		{
+			bool any = false;
+			do {
+				for (int dir=0;dir<4;dir++)
+				{
+					if ((allowed & (1<<dir)) && rand()%4 == 0)
+					{
+						int nbridge = (rand()%2 ? 2 : 1);
+						fill_until(ret, width, y, x, dir, '#', nbridge);
+						newtile += nbridge;
+						any = true;
+					}
+				}
+			} while (!any);
+		}
+		ret[ix] = newtile;
+		
+		fill_until(ret, width, y, x, 0, '.');
+		fill_until(ret, width, y, x, 1, '.');
+		fill_until(ret, width, y, x, 2, '.');
+		fill_until(ret, width, y, x, 3, '.');
+		
+		return true;
 	}
 	
 public:
@@ -637,7 +712,7 @@ public:
 		//space - empty
 		//0-8 - number of bridges
 		//# - the intended solution has a bridge here
-		//>^<v - can place island here, bridge is in the given direction
+		//. - can place island here, look for bridges in all four directions
 		//\n - line separator
 		
 		add_island(ret, width, y, x);
@@ -645,25 +720,27 @@ public:
 		
 		while (islands)
 		{
+//puts(ret);
 			int nslots = 0;
 			int nix = 0;
 			for (unsigned ix=0;ix<ret.length();ix++)
 			{
-				if (strchr(">^<v", ret[ix]))
+				if (ret[ix] == '.')
 				{
 					nslots++;
 					if (rand() % nslots == 0) nix = ix;
 				}
 			}
 			
-			if (nslots != 0) add_island(ret, width, nix/(width+1), nix%(width+1));
+			if (nslots == 0) break;
 			
-			islands--;
+			bool ok = add_island(ret, width, nix/(width+1), nix%(width+1));
+			if (ok) islands--;
 		}
 		
 		for (unsigned ix=0;ix<ret.length();ix++)
 		{
-			if (strchr(">^<v#", ret[ix])) ret[ix] = ' ';
+			if (ret[ix] == '.' || ret[ix] == '#') ret[ix] = ' ';
 		}
 		
 		return ret;
@@ -712,7 +789,7 @@ srand(0);
 			{
 				puts("ERROR");
 				puts(map);
-				exit(0);
+				exit(1);
 			}
 			if (result == -1) nunsolv++;
 			if (result == 0) nunsolv++;
@@ -725,4 +802,19 @@ srand(0);
 	printf("%i solvable, %i unsolvable, %i multiple solutions\n", nsolv, nunsolv, nmultisolv);
 	
 	return 0;
+}
+
+test("generator","solver","generator")
+{
+	for (int i=0;i<=1000;i++)
+	{
+		if (i == 1000) assert(!"game generator needs to contain loops in its intended solutions");
+		string game = generator::generate(2, 2, 4);
+		if (game == "22\n22\n") break;
+		if (game == "44\n44\n") break;
+		if (game == "33\n44\n") break;
+		if (game == "44\n33\n") break;
+		if (game == "34\n34\n") break;
+		if (game == "43\n43\n") break;
+	}
 }
